21dc960a7a898729c36285bb731863072c3a758e
ryan02-22 <ryan73147@gmail.com>
2025-12-19 21:30:13 +0700

    merging homepage and main main pages latest

--- app/src/main/java/com/example/homepage/ProfilActivity.java
+++ app/src/main/java/com/example/homepage/ProfilActivity.java
@@ -5,6 +5,11 @@ import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfilActivity extends AppCompatActivity {
 
@@ -20,5 +25,18 @@ public class ProfilActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        RecyclerView recyclerView = findViewById(R.id.profile_items_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<ProfileItem> profileItems = new ArrayList<>();
+        profileItems.add(new ProfileItem(R.drawable.ic_email, R.color.email_icon_background, R.color.email_icon_tint, "Email", "johndoe@email.com"));
+        profileItems.add(new ProfileItem(R.drawable.ic_phone, R.color.phone_icon_background, R.color.phone_icon_tint, "Nomor Telepon", "+62 812-3456-7890"));
+        profileItems.add(new ProfileItem(R.drawable.ic_date, R.color.date_icon_background, R.color.date_icon_tint, "Tanggal Lahir", "15 Januari 1990"));
+        profileItems.add(new ProfileItem(R.drawable.ic_location, R.color.location_icon_background, R.color.location_icon_tint, "Alamat", "Jl. Kesehatan No. 123, Jakarta"));
+        profileItems.add(new ProfileItem(R.drawable.ic_work, R.color.work_icon_background, R.color.work_icon_tint, "Pekerjaan", "Karyawan Swasta"));
+
+        ProfileAdapter adapter = new ProfileAdapter(profileItems);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/homepage/ProfileAdapter.java
@@ -0,0 +1,63 @@
+package com.example.homepage;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder> {
+
+    private final List<ProfileItem> items;
+
+    public ProfileAdapter(List<ProfileItem> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ProfileViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_profile_info, parent, false);
+        return new ProfileViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProfileViewHolder holder, int position) {
+        ProfileItem item = items.get(position);
+        Context context = holder.itemView.getContext();
+
+        holder.icon.setImageResource(item.getIcon());
+        holder.icon.setColorFilter(ContextCompat.getColor(context, item.getIconTintColor()));
+        holder.iconContainer.setCardBackgroundColor(ContextCompat.getColor(context, item.getIconBackgroundColor()));
+        holder.label.setText(item.getLabel());
+        holder.value.setText(item.getValue());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static class ProfileViewHolder extends RecyclerView.ViewHolder {
+        final CardView iconContainer;
+        final ImageView icon;
+        final TextView label;
+        final TextView value;
+
+        public ProfileViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iconContainer = itemView.findViewById(R.id.icon_container);
+            icon = itemView.findViewById(R.id.iv_icon);
+            label = itemView.findViewById(R.id.tv_label);
+            value = itemView.findViewById(R.id.tv_value);
+        }
+    }
+}
