0be391754be414a24802402194763f1a679c2be0
vinaekalayalani <vinaekalayalani@gmail.com>
2025-12-19 20:20:18 +0700

    create page login register page

---
+++ app/src/main/java/com/vinaekal/sisehat/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.vinaekal.sisehat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView textSubDescription;
+    EditText editEmail, editPassword;
+    Button buttonLogin;
+
+    @Override
+    protected void  onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textSubDescription = findViewById(R.id.textSubDescription);
+
+        buttonLogin.setOnClickListener(v -> {
+            String email = editEmail.getText().toString().trim();
+            String password = editPassword.getText().toString().trim();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Email dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Login berhasil (dummy)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        textSubDescription.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
