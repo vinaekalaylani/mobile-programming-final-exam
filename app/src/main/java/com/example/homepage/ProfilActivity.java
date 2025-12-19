c36580a537bd96d648303efabddc175a2e825b8a
ryan02-22 <ryan73147@gmail.com>
2025-12-19 18:52:36 +0700

    update
    # Conflicts:
    #	.idea/deploymentTargetSelector.xml
    #	.idea/misc.xml
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values-night/themes.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/themes.xml
    #	gradle/libs.versions.toml
    #	gradle/wrapper/gradle-wrapper.jar
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew
    #	gradlew.bat
    #	settings.gradle.kts

---
+++ app/src/main/java/com/example/homepage/ProfilActivity.java
@@ -0,0 +1,24 @@
+package com.example.homepage;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+
+        ImageView backArrow = findViewById(R.id.back_arrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
